cb5142b68612e3cb792e37856545649dcd60abda
drzl777 <andrew.zheng725@gmail.com>
2017-07-15 13:45:14 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends (state, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      if (action.friend) {
+        const newFriend = {...action.friend}
+        return {...state, friends: [...state.friends, newFriend ]}
+      }
+    case 'REMOVE_FRIEND':
+      if (action.id) {
+        const removedFriendList = [...state.friends].filter(friend => friend.id !== action.id)
+        return {...state, friends: removedFriendList}
+      }
+    default:
+      return {...state}
+  }
 }
